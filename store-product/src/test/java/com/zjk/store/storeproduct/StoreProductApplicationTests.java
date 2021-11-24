8f6a725b150073aa39811921666e59e69f4cfa3b
zjk <zjk@qq.com>
2021-11-25 00:02:46 +0800

    first commit

---
+++ store-product/src/test/java/com/zjk/store/storeproduct/StoreProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjk.store.storeproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
